4c409ac576b1fa17c15a1020a1a6ca3c4324da78
Tharun Kumarr A <tharunkumarra@gmail.com>
2023-12-15 11:00:16 +0530

    added insert main vue component

---
+++ Frontend/src/components/Admin/Insert/Insert.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-frame">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  },
+};
+</script>
+
+<style scoped>
+.main-frame{
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+</style>
